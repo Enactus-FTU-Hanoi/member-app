db1a91f12a680e3dd32d4ede94e730da5c2a3f52
Tu Hoang Minh <tuhm@vief.edu.vn>
2026-05-14 02:14:02 +0700

    fix: cors + mime type

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    fs: {
-      strict: false,
-    }
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(
+      'https://api.enactusftuhanoi.id.vn'
+    ),
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
-    target: 'es2020',
-    minify: 'terser',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        }
-      }
-    }
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
   },
 })
